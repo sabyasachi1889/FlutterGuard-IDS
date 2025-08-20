44e08bc32ccd7e49d9c92849dcc9ad5c0e505d7f
SABYASACHI GANGABASI <SABYASACHIGANGABASI75@GMAIL.COM>
2025-08-20 18:56:00 +0530

    Delete android/src directory

--- android/src/main/kotlin/com/flutterguard_ids/app/MainActivity.kt
+++
@@ -1,7 +0,0 @@
-package com.flutterguard_ids.app
-
-import io.flutter.embedding.android.FlutterFragmentActivity
-import io.flutter.plugins.GeneratedPluginRegistrant
-
-class MainActivity: FlutterFragmentActivity() {
-}
