8bd2fe5dc8b78af4e60a4bfcabef989d39ac7b1a
SABYASACHI GANGABASI <SABYASACHIGANGABASI75@GMAIL.COM>
2025-08-20 18:28:45 +0530

    Create MainActivity.kt

---
+++ android/src/main/kotlin/com/flutterguard_ids/app/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.flutterguard_ids.app
+
+import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterFragmentActivity() {
+}
